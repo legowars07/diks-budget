cfb28d1780f8f7b5e2f0fdf64d9bd898645b4861
Raymundo Brunst <72440470+legowars07@users.noreply.github.com>
2025-09-12 14:24:09 +0200

    testing ci

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # diks-budget
 Diks job assessment
+
+## Development
+
+1. Installeer dependencies:
+   ```
+   pnpm install
+   ```
+2. Start de server:
+   ```
+   pnpm exec ts-node src/index.ts
+   ```
---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      globals: {
+        process: 'readonly',
+        console: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      "indent": ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"]
+    },
+  },
+];
